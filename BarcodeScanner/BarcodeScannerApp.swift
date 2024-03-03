c2009039d835b8e5f3629cf8adca1dcb83a21549
Vlad Furtuna <144479033+furtunavlad@users.noreply.github.com>
2024-03-03 15:05:56 +0200

    Initial Commit

---
+++ BarcodeScanner/BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScannerApp.swift
+//  BarcodeScanner
+//
+//  Created by Vlad Furtuna on 03.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
